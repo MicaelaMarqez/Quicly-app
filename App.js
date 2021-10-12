f580e5da487a2b8504e8f6338f53e014f6962fb4
Mica <mica@mica.com>
2021-10-12 17:06:37 -0300

    push

--- App.js
+++ App.js
@@ -9,13 +9,16 @@ import rootReducer from './redux/reducers/rootReducer'
 // import { NativeBaseProvider, Box } from 'native-base';
 
 import { LogBox } from "react-native"
-import Product from './components/Product';
 LogBox.ignoreAllLogs(true)
 
 const globalStore = createStore(rootReducer, applyMiddleware(thunk))
 
 export default function App() {
   return (
-    <Product/>
+    <NavigationContainer>
+      <Provider store={globalStore}>
+        <Navigator />
+      </Provider>
+    </NavigationContainer>
   );
 }
