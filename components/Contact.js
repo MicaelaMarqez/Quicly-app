bd0c3dd680a4d1158b64181c4da105f77b9aa73a
paula rivarola <paularivarola02@gmail.com>
2021-10-14 16:53:39 -0300

    pau

--- components/Contact.js
+++ components/Contact.js
@@ -1,16 +1,22 @@
-import React from 'react'
-import { View, Text, TouchableOpacity, TextInput, StyleSheet, TextArea } from 'react-native'
+import React from "react"
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet,
+  TextArea,
+} from "react-native"
 
 const Contact = () => {
   return (
     <View style={styles.containerInput}>
+      <Text style={styles.title}>Centro de Ayuda</Text>
       <TextInput
-        placeholder='Nombre'
-        placeholderTextColor='#333333'
+        placeholder="Numero alternativo"
+        placeholderTextColor="#8C9197"
         style={styles.input}
-        //   onChangeText={(e) => input(e, "")}
       />
-      <TextInput placeholder='numero alternativo' placeholderTextColor='#333333' style={styles.input} />
       {/* <TextInput
         placeholder="consulta"
         placeholderTextColor="#333333"
@@ -19,52 +25,68 @@ const Contact = () => {
       <TextInput
         multiline={true}
         numberOfLines={10}
-        placeholderTextColor='#333333'
-        placeholder='Escribe tu consulta'
+        placeholderTextColor="#8C9197"
+        placeholder="Escribe tu consulta"
         style={{
           height: 200,
-          textAlignVertical: 'top',
+          textAlignVertical: "top",
           borderWidth: 1,
           width: 300,
           borderRadius: 6,
           marginBottom: 10,
-          color: '#000',
-          fontSize: 20,
+          paddingTop: 6,
+          color: "#8C9197",
+          fontSize: 17,
+          borderColor: "#8C9197",
           paddingLeft: 15,
         }}
       />
 
-      <TouchableOpacity style={styles.button}>
-        <Text style={{ color: '#fff' }}> Enviar</Text>
+      <TouchableOpacity
+        onPress={() => props.navigation.navigate("home")}
+        style={styles.button}
+      >
+        <Text style={{ color: "white", fontSize: 22, textAlign: "center" }}>
+          Enviar
+        </Text>
       </TouchableOpacity>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+  title: {
+    color: "tomato",
+    fontSize: 30,
+    textAlign: "center",
+    fontWeight: "bold",
+    justifyContent: "center",
+    padding: 15,
+  },
   containerInput: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
 
   button: {
-    backgroundColor: '#FE6849',
-    width: '35%',
-    borderRadius: 20,
+    backgroundColor: "#FE6849",
+    width: "35%",
+    borderRadius: 50,
     padding: 15,
   },
 
   input: {
-    backgroundColor: 'rgba(255, 255, 255, 0.50)',
     width: 300,
     borderRadius: 6,
+    color: "#8C9197",
     paddingBottom: 5,
     paddingLeft: 15,
     paddingRight: 15,
     paddingTop: 6,
     marginBottom: 10,
-    fontSize: 20,
+    fontSize: 17,
     borderWidth: 1,
+    borderColor: "#8C9197",
   },
 })
 
